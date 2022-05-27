442dff663cf7ae6921579156543b67d9730c50c9
PashaTravkin <refueller2022@bk.ru>
2022-05-28 00:02:48 +0300

    learn method map

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,28 @@ import './App.css';
 import {Accordion} from "./Components/accordion/Accordion";
 import {Rating} from "./Components/rating/Rating";
 import {PageTitle} from "./Components/appTitle/PageTitle";
+import {Map1} from "./Components/Method_Map/Map1";
 import {Map} from "./Components/Method_Map/Map";
 
 const topCars = [
-    {manufacturer:'BMW', model:'m5cs'},
-    {manufacturer:'Mercedes', model:'e63s'},
-    {manufacturer:'Audi', model:'rs6'},
-    {manufacturer:'BMW', model:'m5cs'},
-    {manufacturer:'Mercedes', model:'e63s'},
-    {manufacturer:'Audi', model:'rs6'}
+    {manufacturer: 'BMW', model: 'm5cs'},
+    {manufacturer: 'Mercedes', model: 'e63s'},
+    {manufacturer: 'Audi', model: 'rs6'},
+    {manufacturer: 'BMW', model: 'm5cs'},
+    {manufacturer: 'Mercedes', model: 'e63s'},
+    {manufacturer: 'Audi', model: 'rs6'}
 ]
 
 function App() {
 
     return (
         <span>
-           <PageTitle title = {"This is App component"}/>
-           <PageTitle title = {"My friends"}/>
+           <PageTitle title={"This is App component"}/>
+           <PageTitle title={"My friends"}/>
             Article1
             <Rating value={5}/>
-            <Accordion titleValue = {"Menu"} collapsed = {true} />
-            <Accordion titleValue = {"Users"} collapsed = {false}/>
+            <Accordion titleValue={"Menu"} collapsed={true}/>
+            <Accordion titleValue={"Users"} collapsed={false}/>
             Article2
             <Rating value={0}/>
             <Rating value={1}/>
@@ -31,7 +32,8 @@ function App() {
             <Rating value={3}/>
             <Rating value={4}/>
             <Rating value={5}/>
-            <Map topCars = {topCars}/>
+            <Map topCars={topCars}/>
+            <Map1 topCars={topCars}/>
     </span>
     );
 }
